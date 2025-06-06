5864dc221e850ad70c558c5825521342b836d0c0
Lyncoln Mauricio <lyncolnms@gmail.com>
2025-06-05 22:18:00 -0300

    Demonstrates optional handling in Swift
    Illustrates how to safely unwrap an optional String
    using both nil coalescing and optional binding.

---
+++ DesafioPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+let name = "Steve"
+var surname: String? = "Jobs"
+
+print("\(name) \(surname ?? "Wozniak")")
+
+if let surname = surname {
+    print("\(name) \(surname)")
+} else {
+    print("\(name) Wozniak")
+}
